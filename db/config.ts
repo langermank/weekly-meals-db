1ce2e25e75ff99e86943a8ac063a45bcb7561d03
langermank <18661030+langermank@users.noreply.github.com>
2024-03-23 16:42:28 -0700

    set up db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,47 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
-// https://astro.build/db/config
-export default defineDb({
-	tables: {},
+export const Weeks = defineTable({
+	columns: {
+		week_id: column.number({ primaryKey: true }),
+		start_date: column.date(),
+		end_date: column.date(),
+	}
 });
+
+export const Days = defineTable({
+	columns: {
+		day_id: column.number({ primaryKey: true }),
+		date: column.date(),
+		week_id: column.number({ references: () => Weeks.columns.week_id }),
+	}
+});
+
+export const Meals = defineTable({
+	columns: {
+		meal_id: column.number({ primaryKey: true }),
+		day_id: column.number({ references: () => Days.columns.day_id }),
+		name: column.text(),
+	}
+});
+
+export const Foods = defineTable({
+	columns: {
+		food_id: column.number({ primaryKey: true }),
+  	name: column.text(),
+  	label_image: column.text(),
+	}
+});
+
+export const MealFoods = defineTable({
+	columns: {
+		meal_food_id: column.number({ primaryKey: true }),
+		meal_id: column.number({ references: () => Meals.columns.meal_id }),
+		food_id: column.number({ references: () => Foods.columns.food_id }),
+		quantity_user1: column.number(),
+		quantity_user2: column.number(),
+	}
+});
+
+export default defineDb({
+  tables: { Weeks, Days, Meals, Foods, MealFoods },
+})
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,23 @@
-import { db } from 'astro:db';
+import { db, Weeks, Days, Meals, Foods, MealFoods } from 'astro:db';
 
-export default async function seed() {
-	// Seed local development data.
-	// See https://docs.astro.build/en/guides/astro-db/#seed-your-database
+export default async function() {
+  await db.insert(Weeks).values([
+  { week_id: 1, start_date: new Date('2024-03-01'), end_date: new Date('2024-03-07') },
+])
+
+await db.insert(Days).values([
+  { day_id: 1, date: new Date('2024-03-01'), week_id: 1 },
+])
+
+await db.insert(Foods).values([
+  { food_id: 1, name: 'Oatmeal', label_image: 'path/to/oatmeal.jpg' },
+])
+
+await db.insert(Meals).values([
+  { meal_id: 1, day_id: 1, name: 'Breakfast' },
+])
+
+await db.insert(MealFoods).values([
+  { meal_food_id: 1, meal_id: 1, food_id: 1, quantity_user1: 100, quantity_user2: 150 },
+])
 }
