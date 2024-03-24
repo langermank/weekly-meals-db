559cfefe1f4f30d04e225f4b39222693ad17cbac
langermank <18661030+langermank@users.noreply.github.com>
2024-03-23 17:01:11 -0700

    test

--- .netlify/functions-internal/ssr/chunks/generic_DoOy2Dnc.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_CUPPv6eF.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
-  output: "server",
-  adapter: netlify()
+	integrations: [db()],
 });
