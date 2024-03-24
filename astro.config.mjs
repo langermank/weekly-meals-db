28e8059c33172f5b51a2b3ed54510094fff3c8b1
langermank <18661030+langermank@users.noreply.github.com>
2024-03-23 17:39:24 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db()],
+  integrations: [db()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
