7b9579806b5beb5520c87841b0fe17bdd60c3782
langermank <18661030+langermank@users.noreply.github.com>
2024-03-26 14:47:19 -0700

    fun with kiran!

--- db/config.ts
+++ db/config.ts
@@ -16,41 +16,18 @@ export const Days = defineTable({
 	}
 });
 
-export const MealTypes = defineTable({
-  columns: {
-    meal_type_id: column.number({ primaryKey: true }),
-    type_name: column.text(), // "Lunch" or "Dinner"
-  }
-});
-
-export const Foods = defineTable({
-	columns: {
-		food_id: column.number({ primaryKey: true }),
-  	name: column.text(),
-		macrofactor_url: column.text({ optional: true }),
-	}
-});
-
 export const Meals = defineTable({
 	columns: {
 		meal_id: column.number({ primaryKey: true }),
 		name: column.text(),
-		meal_type_id: column.number({ references: () => MealTypes.columns.meal_type_id, default: 1}),
+		meal_type: column.text(),
 		day_id: column.number({ references: () => Days.columns.day_id }),
-	}
-});
-
-export const MealFoods = defineTable({
-	columns: {
-		meal_food_id: column.number({ primaryKey: true }),
-		meal_id: column.number({ references: () => Meals.columns.meal_id }),
-		food_id: column.number({ references: () => Foods.columns.food_id }),
-		quantity_katie: column.number({ optional: true }),
-		quantity_taran: column.number({ optional: true }),
+		macrofactor_url: column.text({ optional: true }),
+		notes: column.text({ optional: true }),
 	}
 });
 
 
 export default defineDb({
-  tables: { Weeks, Days, Meals, Foods, MealFoods, MealTypes },
+  tables: { Weeks, Days, Meals },
 })
--- db/seed.ts
+++ db/seed.ts
@@ -1,13 +1,8 @@
-import { db, Weeks, Days, Meals, Foods, MealFoods, MealTypes } from 'astro:db';
+import { db, Weeks, Days, Meals } from 'astro:db';
 
 export default async function () {
 	await db.insert(Weeks).values([
-		{ week_id: 1, start_date: new Date('2024-03-01'), end_date: new Date('2024-03-07') },
-	])
-
-	await db.insert(MealTypes).values([
-		{ meal_type_id: 1, type_name: 'Lunch' },
-		{ meal_type_id: 2, type_name: 'Dinner' },
+		{ week_id: 1, start_date: new Date('2024-03-25'), end_date: new Date('2024-03-31') },
 	])
 
 	await db.insert(Days).values([
@@ -20,66 +15,20 @@ export default async function () {
 		{ day_id: 7, date: new Date('2024-03-31'), week_id: 1 },
 	])
 
-	await db.insert(Foods).values([
-		{ food_id: 1, name: 'Oatmeal', macrofactor_url: '/' },
-		{ food_id: 2, name: 'Banana', macrofactor_url: '/' },
-		{ food_id: 3, name: 'Protein Powder', macrofactor_url: '/' },
-		{ food_id: 4, name: 'Milk', macrofactor_url: '/' },
-		{ food_id: 5, name: 'Greek Yogurt', macrofactor_url: '/' },
-		{ food_id: 6, name: 'Berries', macrofactor_url: '/' },
-		{ food_id: 7, name: 'Peanut Butter', macrofactor_url: '/' },
-		{ food_id: 8, name: 'Bread', macrofactor_url: '/' },
-		{ food_id: 9, name: 'Chicken', macrofactor_url: '/' },
-		{ food_id: 10, name: 'Rice', macrofactor_url: '/' },
-		{ food_id: 11, name: 'Broccoli', macrofactor_url: '/' },
-		{ food_id: 12, name: 'Salmon', macrofactor_url: '/' },
-		{ food_id: 13, name: 'Sweet Potato', macrofactor_url: '/' },
-		{ food_id: 14, name: 'Asparagus', macrofactor_url: '/' },
-		{ food_id: 15, name: 'Beef', macrofactor_url: '/' },
-		{ food_id: 16, name: 'Pasta', macrofactor_url: '/' },
-		{ food_id: 17, name: 'Eggs', macrofactor_url: '/' },
-		{ food_id: 18, name: 'Avocado', macrofactor_url: '/' },
-		{ food_id: 19, name: 'Almonds', macrofactor_url: '/' },
-		{ food_id: 20, name: 'Tuna', macrofactor_url: '/' },
-		{ food_id: 21, name: 'Cottage Cheese', macrofactor_url: '/' },
-		{ food_id: 22, name: 'Turkey', macrofactor_url: '/' },
-		{ food_id: 23, name: 'Pork', macrofactor_url: '/' },
-		{ food_id: 24, name: 'Lentils', macrofactor_url: '/' },
-		{ food_id: 25, name: 'Quinoa', macrofactor_url: '/' },
-	])
-
 	await db.insert(Meals).values([
-		{ meal_id: 1, meal_type_id: 1, name: 'Flatbread pizza', day_id: 1 },
-		{ meal_id: 2, meal_type_id: 2, name: 'Chicken and rice', day_id: 1 },
-		{ meal_id: 3, meal_type_id: 1, name: 'Oatmeal', day_id: 2 },
-		{ meal_id: 4, meal_type_id: 2, name: 'Protein shake', day_id: 2 },
-		{ meal_id: 5, meal_type_id: 1, name: 'Greek yogurt', day_id: 3 },
-		{ meal_id: 6, meal_type_id: 2, name: 'Chicken salad', day_id: 3 },
-		{ meal_id: 7, meal_type_id: 1, name: 'Salmon and sweet potato', day_id: 4 },
-		{ meal_id: 8, meal_type_id: 2, name: 'Oatmeal', day_id: 4 },
-		{ meal_id: 9, meal_type_id: 1, name: 'Protein shake', day_id: 5 },
-		{ meal_id: 10, meal_type_id: 2, name: 'Greek yogurt', day_id: 5},
-		{ meal_id: 11, meal_type_id: 1, name: 'Chicken salad', day_id: 6},
-		{ meal_id: 12, meal_type_id: 2, name: 'Salmon and sweet potato', day_id: 6},
-		{ meal_id: 13, meal_type_id: 1, name: 'Oatmeal', day_id: 7},
-		{ meal_id: 14, meal_type_id: 2, name: 'Protein shake', day_id: 7},
-	])
-
-	await db.insert(MealFoods).values([
-		{ meal_food_id: 1, meal_id: 1, food_id: 1, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 2, meal_id: 2, food_id: 2, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 3, meal_id: 3, food_id: 3, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 4, meal_id: 4, food_id: 4, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 5, meal_id: 5, food_id: 5, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 6, meal_id: 6, food_id: 6, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 7, meal_id: 7, food_id: 7, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 8, meal_id: 8, food_id: 8, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 9, meal_id: 9, food_id: 9, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 10, meal_id: 10, food_id: 10, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 11, meal_id: 11, food_id: 11, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 12, meal_id: 12, food_id: 12, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 13, meal_id: 13, food_id: 13, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 14, meal_id: 14, food_id: 14, quantity_katie: 100, quantity_taran: 150 },
-		{ meal_food_id: 15, meal_id: 1, food_id: 15, quantity_katie: 100, quantity_taran: 150 },
+		{ meal_id: 1, meal_type: 'lunch', name: 'Flatbread pizza', day_id: 1, macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
+		{ meal_id: 2, meal_type: 'dinner', name: 'Chicken and rice', day_id: 1, macrofactor_url: 'https://macrofactor.com/chicken-and-rice'},
+		{ meal_id: 3, meal_type: 'dinner', name: 'Oatmeal', day_id: 2, macrofactor_url: 'https://macrofactor.com/oatmeal'},
+		{ meal_id: 4, meal_type: 'dinner', name: 'Protein shake', day_id: 2, macrofactor_url: 'https://macrofactor.com/protein-shake'},
+		{ meal_id: 5, meal_type: 'dinner', name: 'Greek yogurt', day_id: 3 },
+		{ meal_id: 6, meal_type: 'dinner', name: 'Chicken salad', day_id: 3 },
+		{ meal_id: 7, meal_type: 'dinner', name: 'Salmon and sweet potato', day_id: 4 },
+		{ meal_id: 8, meal_type: 'dinner', name: 'Oatmeal', day_id: 4 },
+		{ meal_id: 9, meal_type: 'dinner', name: 'Protein shake', day_id: 5 },
+		{ meal_id: 10, meal_type: 'dinner', name: 'Greek yogurt', day_id: 5},
+		{ meal_id: 11, meal_type: 'dinner', name: 'Chicken salad', day_id: 6},
+		{ meal_id: 12, meal_type: 'dinner', name: 'Salmon and sweet potato', day_id: 6},
+		{ meal_id: 13, meal_type: 'dinner', name: 'Oatmeal', day_id: 7},
+		{ meal_id: 14, meal_type: 'dinner', name: 'Protein shake', day_id: 7},
 	])
 }
