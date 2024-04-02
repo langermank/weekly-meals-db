41f75abd921b9e71fa6d23c5f5a0ab2727ff4577
langermank <18661030+langermank@users.noreply.github.com>
2024-04-02 07:28:15 -0700

    fix day of week logic

--- db/seed.ts
+++ db/seed.ts
@@ -2,7 +2,7 @@ import { db, Meals } from 'astro:db';
 
 export default async function () {
 	await db.insert(Meals).values([
-		{ meal_id: 1, meal_type: 'lunch', name: 'Flatbread pizza', day: new Date('2024-03-25'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
-		{ meal_id: 2, meal_type: 'dinner', name: 'Flatbread pizza', day: new Date('2024-03-25'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
+		{ meal_id: 1, meal_type: 'lunch', name: 'Flatbread pizza', day: new Date('2024-04-01'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
+		{ meal_id: 2, meal_type: 'dinner', name: 'Pasta', day: new Date('2024-04-02'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
 	])
 }
