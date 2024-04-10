ca5171ed3ebe15c02d2865d870e52e9b8b380513
langermank <18661030+langermank@users.noreply.github.com>
2024-04-09 19:23:19 -0700

    lock

--- db/seed.ts
+++ db/seed.ts
@@ -2,7 +2,7 @@ import { db, Meals } from 'astro:db';
 
 export default async function () {
 	await db.insert(Meals).values([
-		{ meal_id: 1, meal_type: 'lunch', name: 'Flatbread pizza', day: new Date('2024-04-01T20:00:00'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
-		{ meal_id: 2, meal_type: 'dinner', name: 'Pasta', day: new Date('2024-04-02T20:00:00'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
+		{ meal_id: 1, meal_type: 'lunch', name: 'Flatbread pizza', day: new Date('2024-04-08T05:00:00'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
+		{ meal_id: 2, meal_type: 'dinner', name: 'Pasta', day: new Date('2024-04-09T05:00:00'), macrofactor_url: 'https://macrofactor.com/flatbread-pizza', notes: 'Use whole wheat flatbread' },
 	])
 }
