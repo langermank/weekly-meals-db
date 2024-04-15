89c657ff26a6c763e2bb55bb68e34b58e5b6fe45
langermank <18661030+langermank@users.noreply.github.com>
2024-04-15 08:47:03 -0700

    make it pop

--- db/seed.ts
+++ db/seed.ts
@@ -12,6 +12,10 @@ import { db, Meals } from 'astro:db';
 export default async function() {
   await db.insert(Meals).values([
 		{ meal_id: 1, meal_type: 'Lunch', name: 'Pizza', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W15', dayOfWeek: 'Monday' },
-		{ meal_id: 2, meal_type: 'Dinner', name: 'Pizza', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W15', dayOfWeek: 'Monday'},
+		{ meal_id: 2, meal_type: 'Dinner', name: 'Pizza', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W15', dayOfWeek: 'Monday' },
+		{ meal_id: 3, meal_type: 'Dinner', name: 'Pizza', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W15', dayOfWeek: 'Tuesday' },
+		{ meal_id: 4, meal_type: 'Dinner', name: 'Test', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W16', dayOfWeek: 'Monday' },
+		{ meal_id: 5, meal_type: 'Dinner', name: 'Test', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W16', dayOfWeek: 'Tuesday'},
+		{ meal_id: 6, meal_type: 'Lunch', name: 'Test', day: new Date('2024-04-08T05:00:00'), macrofactor_url: '/', notes: 'Some notes', week: '2024-W16', dayOfWeek: 'Monday'},
   ])
 }
